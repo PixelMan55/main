6454f21dac6ddb17cfc5f136daf243be356e6f7a
PixelMan55 <161087471+PixelMan55@users.noreply.github.com>
2025-03-19 17:45:46 -0400

    Update scritpt4.js

--- scritpt4.js
+++ scritpt4.js
@@ -1,7 +1,14 @@
 function indexteleport(){
     window.location.href = 'index.html'
 }
-function check(){
-  code = document.getElementById("access-code").value;
-    if (code == "pxz56") 
-       window.location.href = 'pxz56-code-club.html';}
+function indexteleport(){
+    window.location.href = 'index.html'
+}
+function check() {
+    code = document.getElementById("access-code").value;
+    if (code == "pxz56") {
+        window.location.href = 'pxz56-code-club.html';
+    }
+    else {
+        console.log(code);
+    }
