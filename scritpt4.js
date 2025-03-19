59595f5223c5e1fcf7c61d673ab73a6be548953d
PixelMan55 <161087471+PixelMan55@users.noreply.github.com>
2025-03-19 17:22:25 -0400

    Update scritpt4.js

--- scritpt4.js
+++ scritpt4.js
@@ -1,7 +1,7 @@
 function indexteleport(){
     window.location.href = 'index.html'
 }
-function code(){
+function check(){
   code = document.getElementById("access-code").value;
     if (code == "pxz56") 
        window.location.href = 'pxz56-code.club.html';}
